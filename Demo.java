6691624df9a5ca24bc8735229c0ca6907f84220b
Rani Bhatia <ranibhatia220@gmail.com>
2022-02-09 19:01:23 +0530

    my first commit

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+   public static void main( String args[] ) {
+      int a = 10;
+      int b = 20;
+      int c = 25;
+      System.out.println("b / a = " + (b / a) );
+      System.out.println("b % a = " + (b % a) );
+      c /= a ;
+      System.out.println("c /= a = " + c );
+   }
+}

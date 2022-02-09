0a111cf6c575c9d95658aef6890fa3cbd27759cf
Rani Bhatia <ranibhatia220@gmail.com>
2022-02-09 19:04:11 +0530

    updted code corrected

--- Demo.java
+++ Demo.java
@@ -1,7 +1,7 @@
 public class Demo {
    public static void main( String args[] ) {
       int a = 10;
-      int b = 0;
+      int b = 20;
       int c = 25;
       System.out.println("b / a = " + (b / a) );
       System.out.println("b % a = " + (b % a) );

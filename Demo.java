54dd9ea177363318ccac0edeaeda720e91a5e5e2
Rani Bhatia <ranibhatia220@gmail.com>
2022-02-10 10:42:42 +0530

    Revert "4th commit"
    This reverts commit c291809fcc2304144a4066c1b4d3a4a272b54fb9.

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
 public class Demo {
    public static void main( String args[] ) {
-      int a = 10;
+      int a = 0;
       int b = 20;
       int c = 25;
       System.out.println("b / a = " + (b / a) );
